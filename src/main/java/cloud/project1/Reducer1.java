2749a34eefbb47bbe67e238740e160a2cf71f49b
dhruvraj.parmar <dhruvraj.parmar@gmail.com>
2015-11-19 21:56:26 -0700

    Location Update and Reducer output value changes.

--- src/main/java/cloud/project1/Reducer1.java
+++ src/main/java/cloud/project1/Reducer1.java
@@ -8,31 +8,36 @@ import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Reducer;
 import twitter4j.Status;
 
-public class Reducer1 extends Reducer<Text,CustomWritable,Text,IntWritable>{
+public class Reducer1 extends Reducer<Text,CustomWritable,Text,Text>{
 
-	HashMap<String,String> place=new HashMap<String,String>();
-	HashMap<String,Integer> plcount=new HashMap<String,Integer>();
 	@Override
 	public void reduce(Text arg0, Iterable<CustomWritable> arg1,
 			Context context) throws IOException, InterruptedException {
+
+		HashMap<String,String> place=new HashMap<String,String>();
+		HashMap<String,Integer> plcount1=new HashMap<String,Integer>();
+		ValueComparator vcm=new ValueComparator(plcount1);
+		TreeMap<String,Integer> plcount2=new TreeMap<String,Integer>(vcm);
+//		List<Integer> countl=new ArrayList<Integer>();
+		ArrayList<Text> outlist=new ArrayList<Text>();
 		int count=0,total=0;
 		int follower=0,placecount=0;
 		for(CustomWritable s:arg1)
 		{
 			try{
-					System.out.println("Inside Reducer1");
+					System.out.println("Inside Reducer2");
 					follower=follower+s.getValue().get();
 					count++;
-/*					String plc=s.getPlace().toString();
+					String plc=s.getPlace().toString();
+					if(plcount1.containsKey(plc))
 					{
-					if(place.containsKey(plc))
-						placecount=placecount+1;
-						plcount.put(plc, placecount);
+						placecount=plcount1.get(plc)+1;
+						plcount1.put(plc,placecount);
 					}
 					else
 					{
-						place.put(plc, arg0.toString());
-					}*/
+						plcount1.put(plc, 1);
+					}
 							
 				}
 			catch(Exception e)
@@ -43,12 +48,28 @@ public class Reducer1 extends Reducer<Text,CustomWritable,Text,IntWritable>{
 		}
 		
 		int fol=follower/count;
+		String strfol=Integer.toString(fol);
+		String strcnt=Integer.toString(count);
+		outlist.add(new Text(strfol));
+		outlist.add(new Text(strcnt));
+		plcount2.putAll(plcount1);
+		System.out.println("Place map"+plcount2);
+		for(int i=0;i<10;i++)
+		{
+			Map.Entry<String, Integer> ent=plcount2.pollFirstEntry();			
+			outlist.add(new Text(ent.getKey()+" "+ent.getValue()));
+		}
+	
 		String out0="Total tweets about " +arg0.toString();
-		context.write(new Text(out0), new IntWritable(count));
 		String out1="Average Influence of people who tweeted about "+arg0.toString();
-		context.write(new Text(out1),new IntWritable(fol));
-		
-
+//		TextArrayWritable taw=new TextArrayWritable(Text.class,outlist.toArray());
+		context.write(new Text(out0), new Text(outlist.get(0)));
+		context.write(new Text(out1), new Text(outlist.get(1)));
+		String out2="Tweet Count for " +arg0.toString()+" at ";
+		for(int i=2;i<12;i++)
+		{
+		context.write(new Text(out2),new Text(outlist.get(i)));	
+		}
 	}
 		
 }
